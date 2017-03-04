642701044a9a26a05b9164307940805186d65d61
Christopher Mattox <metroplex02@gmail.com>
2017-03-04 14:17:19 -0600

    Cohort Controller created

---
+++ public/scripts/controllers/cohort.controller.js
@@ -0,0 +1,20 @@
+/*jshint esversion: 6 */
+angular.module('app')
+  .controller('CohortController', ['$http', function($http){
+    const self = this;
+
+    self.cohort = {};
+
+    function getCohorts(){
+      let promise = $http({
+        method: 'GET',
+        url: '/cohort'
+      }).then(function(response){
+        self.cohort = response.data
+      });
+      return promise;
+    }
+
+    getCohorts();
+    
+}]);
