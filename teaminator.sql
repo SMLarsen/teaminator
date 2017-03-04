702a903ec3bd511404e145f4a1145c3373bab6d1
Steve Larsen <stevelarsen01@gmail.com>
2017-03-04 13:56:34 -0600

    add CRUD queries to team route

--- teaminator.sql
+++ teaminator.sql
@@ -3,6 +3,12 @@ id SERIAL PRIMARY KEY,
 name VARCHAR(50)
 );
 
+INSERT INTO cohort (name)
+VALUES ('Sigma')
+;
+
+SELECT * FROM cohort;
+
 CREATE TABLE person (
 id SERIAL PRIMARY KEY,
 cohort_id INTEGER REFERENCES cohort,
@@ -16,6 +22,12 @@ team_count INTEGER,
 name VARCHAR(50)
 );
 
+INSERT INTO project (cohort_id, team_count, name)
+VALUES (1, 5, 'Sigma Project Teaminator')
+;
+
+SELECT * FROM project;
+
 CREATE TABLE team (
 id SERIAL PRIMARY KEY,
 project_id INTEGER REFERENCES project,
@@ -23,9 +35,17 @@ team_size INTEGER,
 name VARCHAR(50)
 );
 
+INSERT INTO team (project_id, team_size, name)
+VALUES (1, 4, 'Sigma Project Teaminator Team 1')
+;
+
+SELECT * FROM team;
+
 CREATE TABLE team_member (
 id SERIAL PRIMARY KEY,
 team_id INTEGER REFERENCES team,
 person_id INTEGER REFERENCES person
 );
 
+INSERT INTO team (project_id, team_size)
+VALUES (
