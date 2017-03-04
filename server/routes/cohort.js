730841276a3d336ef3cdb42d440d098de86e0238
jasunde <jlaurits@gmail.com>
2017-03-04 14:15:36 -0600

    Get cohort by id, post, put, delete

--- server/routes/cohort.js
+++ server/routes/cohort.js
@@ -16,23 +16,22 @@ router.get("/", function(req, res) {
     });
 });
 
-// get cohort members
-router.get("/:id", function (req, res) {
+// get one cohort
+router.get("/:id", function(req, res) {
   pool.query(
-    'SELECT * FROM cohort WHERE id = $1'
+    'SELECT * FROM cohort WHERE id = $1',
     [req.params.id]
   )
     .then(function (result) {
-      res.send(result.data);
+      res.send(result.rows[0]);
     })
     .catch(function (err) {
       res.status(500).send(err);
     });
-})
+});
 
 // create cohort
 router.post("/", function (req, res) {
-  console.log('req.body', req.body);
   pool.query(
     'INSERT INTO cohort (name) VALUES($1)',
     [req.body.name]
@@ -44,12 +43,30 @@ router.post("/", function (req, res) {
       res.status(500).send(err);
     });
 })
+//
+// get cohort members
+router.put("/", function (req, res) {
+  console.log('req.body', req.body);
+  pool.query(
+    'UPDATE cohort SET name = $1 WHERE id = $2',
+    [req.body.name, req.body.id]
+  )
+    .then(function (result) {
+      res.sendStatus(200);
+    })
+    .catch(function (err) {
+      res.status(500).send(err);
+    });
+})
 
 // delete cohort
 router.delete("/:id", function (req, res) {
-  pool.query('SELECT * FROM cohort')
-    .then(function (result) {
-      res.send(result.data);
+  pool.query(
+    'DELETE FROM cohort WHERE id = $1',
+    [req.params.id]
+  )
+    .then(function (response) {
+      res.sendStatus(200);
     })
     .catch(function (err) {
       res.status(500).send(err);
